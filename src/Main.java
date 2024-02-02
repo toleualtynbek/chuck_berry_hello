0b74c57c6d84383bbda9dc14525fdd407c522ebf
toleu.altinbek <toleu.altinbek@gmail.com>
2024-02-02 17:31:56 +0600

    init commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
+        for (int i=0; i<10; i++)
+        {
+            System.out.println(i);
+        }
     }
 }
